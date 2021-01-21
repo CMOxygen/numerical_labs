0cf49133bff2b2d4ef5fe61286b108d56f2563d0
CMOxygen <75035702+CMOxygen@users.noreply.github.com>
2021-01-21 23:37:54 +0300

    lab_5_14 Lagrange

--- lab_5_14/lab_5_14.cpp
+++ lab_5_14/lab_5_14.cpp
@@ -17,8 +17,42 @@ int main()
 
     const int size = 6;
 
+    double sum = 0;
+    double top = 1;
+    double bot = 1;
+    double x = 0.1845;
+    double P = 0;
+    double temp;
+
     double xy[2][size] = {{0.180, 0.185, 0.190, 0.195, 0.200, 0.205},
                           {5.61543, 5.46693, 5.32634, 5.19304, 5.06649, 4.94619}};
 
-                          
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << xy[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        top = 1;
+        bot = 1;
+
+        for (int j = 0; j < size; j++)
+        {
+            if (j != i)
+            {
+                top *= x - xy[0][j];
+                bot *= xy[0][i] - xy[0][j];
+            }
+        }
+        P+=xy[1][i] * top / bot;
+    }
+    cout << P << endl;
+
+    //system("pause");
 }
